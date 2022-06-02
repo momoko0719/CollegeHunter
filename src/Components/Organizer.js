38c162e7bcf381c598def2c60c67d71ea45a0846
momoko0719 <yifeit2@uw.edu>
2022-06-02 16:18:10 -0700

    update organizer page

--- src/Components/Organizer.js
+++ src/Components/Organizer.js
@@ -31,7 +31,7 @@ export default function Organizer(props) {
     renderCommon = (
       <div>
         <p><strong>Common App Essay:</strong></p>
-        <img src='/img/college/commonapp.png' className="pb-3" alt='common app essay' />
+        <img src='/img/college/commonapp.png' className="essayimg" alt='common app essay' />
       </div>
     )
   }
@@ -41,7 +41,7 @@ export default function Organizer(props) {
       return (
         <div>
           <p><strong>{eachSchool['name'] + ':'}</strong></p>
-          <img src={'/img/college/' + eachSchool['essayImg']} className="pb-3" alt={eachSchool['name']} />
+          <img src={'/img/college/' + eachSchool['essayImg']} className="essayimg" alt={eachSchool['name']} />
         </div>
       )
     }
@@ -50,7 +50,6 @@ export default function Organizer(props) {
   function EssayQuestion() {
     return (
       <div className="part-2">
-
         <div className="card-title">
           <h3>Application Essay Questions</h3>
         </div>
@@ -70,14 +69,27 @@ export default function Organizer(props) {
         <h2>Manage Your Saved Schools</h2>
       </header>
 
-      <main className="organizer-main">
+      {/* <main className="organizer-main">
         <div className="card m-5">
           <div className="card-body">
             <AppDdl />
             <EssayQuestion />
           </div>
         </div>
-      </main>
+      </main> */}
+
+        <div className="card mb-5">
+          <div className="card shadow-sm">
+            <div className="card-body">
+              <div className="row">
+                <div className="col mb-2">
+                  <AppDdl />
+                  <EssayQuestion />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
